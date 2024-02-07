7160e42e16aeb31dcb145af76d1a2fa2d3caeb31
dashagenze <89853583+dashagenze@users.noreply.github.com>
2024-02-07 16:14:45 +0300

    added spending log function
    and subtracting it from savings

--- scriptsSaving/script.js
+++ scriptsSaving/script.js
@@ -1,9 +1,48 @@
 let h1 = document.getElementById('heading');
-let h2 = document.getElementById('h2');
+let h2 = document.querySelectorAll('h2.h2')
+console.log(h2.length)
 
 let img1 = document.getElementById('img1');
 let img2 = document.getElementById('img2');
 let body = document.getElementById('body');
+
+let spends = [];
+
+
+// TWO EVENTS ON SPENDING SUBMIT: LOG EXPENSE AND SUBTRACT FROM SAVES
+
+const SPENDINGS_FORM = document.getElementById('SPENDINGS_FORM')
+
+SPENDINGS_FORM.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(SPENDINGS_FORM);
+    const spend = +(formData.get('spend'));
+
+    spends.unshift(spend);
+
+    let expenses;
+    expenses = document.getElementById('spendingsField');
+    expenses.textContent = spends.join('; ');
+});
+
+SPENDINGS_FORM.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(SPENDINGS_FORM);
+    const spend = +(formData.get('spend'));
+
+    let savingsSum = document.getElementById('savings');
+
+    function minusSpend (sum, spend) {
+        let saved = sum.textContent;
+        saved = +saved
+        function minus(initial, spent) {
+            return +initial - +spent
+        }
+        sum.textContent = minus(saved, spend);
+    }
+    minusSpend(savingsSum, spend);
+});
+
 
 
 // NAME AND AGE FORM
@@ -15,19 +54,15 @@ MAINFORM.addEventListener('submit', (e) => {
 
     const name = formData.get('name');
     const age = formData.get('age');
-    console.log(typeof name)
-    console.log( name)
 
     try {
-        if (name != '') {
-            if (typeof +age == 'number') {
-                if (age >= 18) {
-                    adultStyle(name)
-                } else if (age < 18 && age != '') {
-                    minorStyle(name)
-                } else alertIncorrect()
-            }
-        } else alertIncorrect()
+        if (name !== '') {
+            if (age >= 18) {
+                adultStyle(name)
+            } else if (age < 18 && age !== '') {
+                minorStyle(name)
+            } else alertIncorrect()
+        }
     } catch (err) {console.log(err.name)}
 
 });
@@ -44,7 +79,8 @@ SAVINGS_FORM.addEventListener('submit', (e) => {
     let savingsSum = document.getElementById('savings');
 
     addToElement(savingsSum, addedSum);
-});
+
+})
 
 function addToElement (field, addedSum) {
     let initialSavingsfield = field.textContent;
@@ -61,6 +97,7 @@ function addToElement (field, addedSum) {
 function adultStyle (name) {
     MAINFORM.style.display = 'none';
     SAVINGS_FORM.style.display = 'block';
+    SPENDINGS_FORM.style.display = 'block';
     h1.innerText = `Здрастуйте, ${name}🤝`;
     img1.src = 'pics/offic.png';
     img2.src = 'pics/suitcase.png';
@@ -69,14 +106,19 @@ function adultStyle (name) {
 
     h1.style.fontFamily = 'Times New Roman, serif';
     h1.style.color = '#554E55';
-    h2.style.display = 'block';
-    h2.style.fontFamily = 'Times New Roman, serif';
-    h2.style.color = '#554E55';
+    h2[0].style.display = 'block';
+    h2[0].style.fontFamily = 'Times New Roman, serif';
+    h2[0].style.color = '#554E55';
+    h2[1].style.display = 'block';
+    h2[1].style.fontFamily = 'Times New Roman, serif';
+    h2[1].style.color = '#554E55';
+
 }
 
 function minorStyle (name) {
     MAINFORM.style.display = 'none';
     SAVINGS_FORM.style.display = 'block';
+    SPENDINGS_FORM.style.display = 'block';
     h1.innerText = `Приветик, ${name}🥺`
     img1.src = 'pics/dream.png';
     img2.src = 'pics/pigmoney.png';
@@ -85,9 +127,13 @@ function minorStyle (name) {
 
     h1.style.fontFamily = '"Comic Sans MS", serif';
     h1.style.color = '#EA64E9'
-    h2.style.display = 'block';
-    h2.style.fontFamily = '"Comic Sans MS", serif';
-    h2.style.color = '#EA64E9'
+    h2[0].style.display = 'block';
+    h2[0].style.fontFamily = '"Comic Sans MS", serif';
+    h2[0].style.color = '#EA64E9'
+    h2[1].style.display = 'block';
+    h2[1].style.fontFamily = '"Comic Sans MS", serif';
+    h2[1].style.color = '#EA64E9'
+
 }
 
 function alertIncorrect (){ alert('Пожалуйста, перезагрузите страницу и введите свои данные :)') }

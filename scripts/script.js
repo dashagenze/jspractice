f822b2f806875e375d79b2aff0fff1bedcddae48
dashagenze <89853583+dashagenze@users.noreply.github.com>
2024-02-16 11:05:55 +0300

    worked

--- scripts/script.js
+++ scripts/script.js
@@ -1,87 +1,101 @@
-let btn = document.getElementById('btn');
-
-
-let ttt = document.getElementById('ttt'),
-    parent = document.getElementById('parent');
-
-let object = {
-    'foo': 44,
-}
-
-console.log(object.type);
-
-ttt.addEventListener('click', (e) => {
-    e.stopPropagation()
-    e.currentTarget.style.backgroundColor = 'red';
-
-    console.log(e.type);
-    console.log(e.target);
-    console.log(e.currentTarget);
-
-})
-
-
-
-
+// let btn = document.getElementById('btn');
+//
+//
+// let ttt = document.getElementById('ttt'),
+//     parent = document.getElementById('parent');
+//
+// let object = {
+//     'foo': 44,
+// }
+//
+// console.log(object.type);
+//
+// ttt.addEventListener('click', (e) => {
+//     e.stopPropagation()
+//     e.currentTarget.style.backgroundColor = 'red';
+//
+//     console.log(e.type);
+//     console.log(e.target);
+//     console.log(e.currentTarget);
+//
+// })
+//
 
 
 
+let hunger = 100;
+
+console.log(hunger);
 
 
-// let hunger = document.getElementById('').textContent;
 
 class Pet {
 
     constructor(name) {
         this.name = name;
-        this.hunger = 100;
     }
 
-    // hungerDecrease() {
-    //     setInterval(function decrease(hunger) {
-    //         hunger--
-    //         console.log(this.hunger);
-    //         // let hungerField = document.getElementById('hunger');
-    //         // hungerField.textContent = hunger;
-    //         // if (hungerField.textContent === '0') {
-    //         //     alert('баня умер');
-    //         //     clearInterval(hungerDecrease)
-    //         // }
-    //     }, 1000)
-
-    // hungerDecrease() {
-    //     let hungerDecrease = setInterval(function decrease() {
-    //         hunger--
-    //         let hungerField = document.getElementById('hunger');
-    //         hungerField.textContent = hunger;
-    //         if (hungerField.textContent === '0') {
-    //             alert('баня умер');
-    //             clearInterval(hungerDecrease)
-    //         }
-    //     }, 10000)
-
-        // function feed(fed) {
-        //     hunger += +fed;
-        //     let hungerField = document.getElementById('hunger');
-        //     hungerField.textContent = hunger;
-        // }
+    sayHelo(name) {
+        alert(`привет, ${this.name}!🥺`)
     }
 
 
+    // hungerdecrease = setInterval(decrease,10);
 
-    // sayHelo(name) {
-    //     alert(`привет, ${this.name}!🥺`)
-    // }
+    i = setInterval(function decrease() {
+        hunger--
+        let hungerfield = document.getElementById('hunger')
+        hungerfield.textContent = String(hunger);
+
+        if (hungerfield.textContent === '0') {
+            alert(`${this.name} умер :/`);
+            clearInterval();
+        }
+    }, 100)
+
+
 
 
 }
 
-let bunny = new Pet('Баня')
-bunny.hungerDecrease(bunny.hunger)
 
-btn.onclick = () => { bunny.feed(50);}
+
+
+let bunny = new Pet('Баня')
+
+
+// setInterval(decrease.bind,10);
+
+
+
+
+
+// btn.onclick = () => { bunny.feed(50);}
 
 // bunny.sayHelo();
-console.log(bunny.hunger);
 
 
+
+// hungerDecrease() {
+//     setInterval(function decrease() {
+//         hunger--
+//         console.log(this.hunger);
+//
+//
+//
+//
+//         // let hungerField = document.getElementById('hunger');
+//         // hungerField.textContent = hunger;
+//         // if (hungerField.textContent === '0') {
+//         //     alert('баня умер');
+//         //     clearInterval(hungerDecrease)
+//         // }
+//     }, 1000)}
+
+
+
+// function feed(fed) {
+//     hunger += +fed;
+//     let hungerField = document.getElementById('hunger');
+//     hungerField.textContent = hunger;
+// }

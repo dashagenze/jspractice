748a1bbdc6cb9eaa50f5726039017070ac98390f
dashagenze <89853583+dashagenze@users.noreply.github.com>
2024-02-20 13:47:46 +0300

    constructors and classes

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,51 @@
-/**
- * Описание задачи: нужно написать функцию, которая будет получать этот массив и число N
- * и возвращать новый массив с элементами,
- * которые в исходном повторяются N или более раз
- * Ожидаемый результат: [1, 2, 2, 2, 3, 4, 3, 3, 5, 4] n=2 => [2, 3, 4]
- * Сложность задачи: 2 of 5
- * @param {Array} array - Массив с примитивными значениями
- * @param {?} n - в исходном повторяются N или более раз
- * @returns {Array} - возвращать новый массив с элементами, которые в исходном повторяются N или более раз
- */
+// function Pet (name, kind, isEvil, owner) {
+//     this.name = name;
+//     this.kind = kind;
+//     this.isEvil = isEvil;
+//     this.owner = owner
+//
+//     this.bite = function biteOwner() {
+//         console.log(`${this.name} bit ${this.owner}`)
+//     }
+// }
+//
+// function Person (name, sex, hasPet, pet, petName) {
+//     this.name = name;
+//     this.sex = sex;
+//     this.hasPet = hasPet;
+//     this.pet = Pet.kind;
+//     this.petName = petName;
+// }
+//
+// let noma = new Person('Noma Grimes', 'M', true, bunny.kind, bunny.name);
+//
+// let bunny = new Pet('Пушненок', 'bunny', true, noma.name);
+//
+//
+// //установить единое для всех объектов данного типа свойство
+// Pet.prototype.isAnimal = true;
+//
+// bunny.bite();
 
 
 
-const duplicated = (array, n) => {
-    let obj = {}
+// Реализуйте класс Worker (Работник), который будет иметь
+// следующие свойства: name (имя), surname (фамилия), rate (ставка за день работы),
+// days (количество отработанных дней). Также класс должен иметь метод getSalary(),
+// который будет выводить зарплату работника.
+// Зарплата - это произведение (умножение) ставки rate на количество отработанных дней days.
+// И метод getFullName() - имя и фамилия работника.
 
-    //создать объект из значений и количества их повторений
-    for (let item of array) {
-        obj[item] = obj[item] ? obj[item] + 1 : 1;
-    }
 
-    let result;
-    result = Object.keys(obj).filter((item) => obj[item] >= n);
-    return result
+function Worker(name, surname, rate, days) {
+
 }
+const worker = new Worker('Иван', 'Иванов', 10, 31);
 
+console.log(worker.name); //выведет 'Иван'
+console.log(worker.surname); //выведет 'Иванов'
+console.log(worker.getFullName); //выведет 'Иванов Иван'
+console.log(worker.rate); //выведет 10
+console.log(worker.days); //выведет 31
+console.log(worker.getSalary()); //выведет 310 - то есть 10*31
 
-const data = [1, 2, 2, 2, 3, 4, 3, 3, 5, 4];
-console.log(duplicated(data, 2)); // [2, 3, 4]
